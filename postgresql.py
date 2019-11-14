bb8c85027f6f2199ce2bc395efa202d5fcd5210b
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-15 02:12:23 +0300

    get method for getting token and write it to postgresql

--- postgresql.py
+++ postgresql.py
@@ -1,16 +1,36 @@
 import psycopg2
 import psycopg2.extras
 
-try:
-    conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
-    print('Postgres connected')
-except psycopg2.Error as e:
-    print('Postgres failed:{0}'.format(e))
+def get_user_token(user):
+    try:
+        conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
+        print('Postgres connected')
+    except psycopg2.Error as e:
+        print('Postgres failed:{0}'.format(e))
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT * FROM users WHERE user = %s" % user)
+    result = cur.fetchone()    
+    cur.close()
+    conn.close()    
+    return result
 
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+def create_user(user, token):
+    try:
+        conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
+    except psycopg2.Error as e:
+        print('Postgres failed:{0}'.format(e))
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users(id, login, token, disabled) VALUES (DEFAULT, '{0}', '{1}', DEFAULT)".format(user, token))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return 'user created'
 
-cur.execute("SELECT * FROM users")
-result = cur.fetchall()
-print(result)
-cur.close()
-conn.close()
+# try:
+#     conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
+#     print('Postgres connected')
+# except psycopg2.Error as e:
+#     print('Postgres failed:{0}'.format(e))
+# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+# cur.close()
+# conn.close()
--- server.py
+++ server.py
@@ -1,31 +1,54 @@
+# -*- encoding: utf-8 -*-
 import logging
 from aiohttp import web
 import json
 import mongo
 import postgresql
+import keygen
 
-async def handle(request):
-    response_obj = {'status': 'success'}
-    return web.Response(text=json.dumps(response_obj), status=200)
+async def main_page(request):
+    header = request.headers
+    if 'Name' in header:
+        user = header['Name']
+        token = keygen.hashText(header['Name'])
+        postgresql.create_user(user, token)
+        response_obj = {'your token': str(token)}
+        return web.Response(text=json.dumps(response_obj), status=200)
+    else:
+        response_obj = {'status': 'message_error', 'message': 'give me your Name'}
+        return web.Response(text=json.dumps(response_obj), status=400)
+
+async def check(request):
+    header = request.headers
+    if 'Auth' in header:
+        response_obj = {'status': 'OK'}
+        return web.Response(text=json.dumps(response_obj), status=200)
+    else:
+        response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
+        return web.Response(text=json.dumps(response_obj), status=401)
 
 async def predict(request):
     try:
         user_predict = await request.json()  # get json-like body
-        # print(user_predict['username'])
-        # print(user_predict['scores'])
-        if 'username' in user_predict and 'scores' in user_predict and len(user_predict) == 2:
-            print(user_predict['username'], user_predict['scores'])
-            response_obj = {'status': 'accepted', 'message': 'your predict accepted'}
-            return web.Response(text=json.dumps(response_obj), status=202)
+        header = request.headers
+        if 'Auth' in header:
+            if 'username' and 'scores' in user_predict and len(user_predict) == 2:
+                response_obj = {'status': 'accepted', 'message': 'your predict accepted'}
+                return web.Response(text=json.dumps(response_obj), status=202)
+            else:
+                response_obj = {'status': 'message_error', 'message': 'pls post it into your ass'}
+                return web.Response(text=json.dumps(response_obj), status=400)
         else:
-            response_obj = {'status': 'error', 'message': 'put it into your ass pls'}
-            return web.Response(text=json.dumps(response_obj), status=400)
+            response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
+            return web.Response(text=json.dumps(response_obj), status=401)
     except Exception as e:
-        response_obj = {'status': 'failed', 'message': 'your have PAWS'}
+        print(e)
+        response_obj = {'status': 'failed', 'message': 'you have PAWS'}
         return web.Response(text=json.dumps(response_obj), status=500)
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
+app.add_routes([web.get('/', main_page),
+                web.get('/my', check),
                 web.post('/predict', predict)])
 logging.basicConfig(level=logging.DEBUG)
 web.run_app(app, host='localhost', port=9090)
