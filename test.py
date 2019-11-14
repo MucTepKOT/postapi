166a5246cb344fd90e9a04e890cd7d71b284ffc0
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-15 02:11:07 +0300

    del test

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-data = {'username':'adsfsdf', 'scores': {'21654':'fgdgfdg'}}
-print(len(data))
-data = json.dumps(data)
-r = requests.post('http://localhost:9090/predict', data=data)
-print(r.text)
