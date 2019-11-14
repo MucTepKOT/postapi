488687782822f2fced4fac581b067818aaffadbf
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-15 02:07:54 +0300

    some add

--- initdb.sql
+++ initdb.sql
@@ -3,3 +3,5 @@ DROP DATABASE IF EXISTS postapi_security;
 CREATE DATABASE postapi_security;
 ALTER DATABASE postapi_security OWNER TO muctepkot;
 GRANT ALL PRIVILEGES ON DATABASE postapi_security TO muctepkot;
+GRANT ALL PRIVILEGES ON TABLE users TO muctepkot;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO muctepkot
