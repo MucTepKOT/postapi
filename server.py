0282194ec248ae27ebdd206d81cd4d1976a569eb
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-16 01:24:16 +0300

    add get method for getting predictions and update post method for sending predictions

--- server.py
+++ server.py
@@ -20,24 +20,48 @@ async def main_page(request):
 
 async def check(request):
     header = request.headers
-    if 'Auth' in header:
-        response_obj = {'status': 'OK'}
-        return web.Response(text=json.dumps(response_obj), status=200)
+    if 'Name' and 'Auth' in header:
+        name = header['Name']
+        token = header['Auth']
+        db_token = postgresql.check_user(name)
+        if token == db_token[0]:
+            response_obj = {'predictions' : mongo.my_prediction(name)}
+            return web.Response(text=json.dumps(response_obj), status=200)
+        else:
+            response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
+            return web.Response(text=json.dumps(response_obj), status=401)
     else:
-        response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
-        return web.Response(text=json.dumps(response_obj), status=401)
+        response_obj = {'status': 'message_error', 'message': 'incorrect parametres'}
+        return web.Response(text=json.dumps(response_obj), status=400)
 
 async def predict(request):
     try:
         user_predict = await request.json()  # get json-like body
         header = request.headers
-        if 'Auth' in header:
-            if 'username' and 'scores' in user_predict and len(user_predict) == 2:
-                response_obj = {'status': 'accepted', 'message': 'your predict accepted'}
-                return web.Response(text=json.dumps(response_obj), status=202)
+        if 'Name' and 'Auth' in header:
+            name = header['Name']
+            token = header['Auth']
+            db_token = postgresql.check_user(name)
+            if db_token == None:
+                response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
+                return web.Response(text=json.dumps(response_obj), status=401)
             else:
-                response_obj = {'status': 'message_error', 'message': 'pls post it into your ass'}
-                return web.Response(text=json.dumps(response_obj), status=400)
+                if token == db_token[0]:
+                    if 'username' and 'scores' in user_predict and len(user_predict) == 2:
+                        data = user_predict
+                        inserting = mongo.insert_db(name, data)
+                        if inserting == 'Success':
+                            response_obj = {'status': 'accepted', 'message': 'your predict accepted'}
+                            return web.Response(text=json.dumps(response_obj), status=202)
+                        else:
+                            response_obj = {'status': 'accepted', 'message': 'something went wrong, repeat pls'}
+                            return web.Response(text=json.dumps(response_obj), status=400)
+                    else:
+                        response_obj = {'status': 'message_error', 'message': 'pls post it into your ass'}
+                        return web.Response(text=json.dumps(response_obj), status=400)
+                else:
+                    response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
+                    return web.Response(text=json.dumps(response_obj), status=401)
         else:
             response_obj = {'status': 'auth_error', 'message': 'ti cho psina, kuda polez?'}
             return web.Response(text=json.dumps(response_obj), status=401)
@@ -48,7 +72,7 @@ async def predict(request):
 
 app = web.Application()
 app.add_routes([web.get('/', main_page),
-                web.get('/my', check),
-                web.post('/predict', predict)])
+                web.get('/my_prediction', check),
+                web.post('/do_predict', predict)])
 logging.basicConfig(level=logging.DEBUG)
 web.run_app(app, host='localhost', port=9090)
