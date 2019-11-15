010ea51f129f04df444c77b2554707476b578d9e
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-16 01:22:29 +0300

    fix queries

--- postgresql.py
+++ postgresql.py
@@ -1,18 +1,20 @@
 import psycopg2
 import psycopg2.extras
 
-def get_user_token(user):
+def check_user(user):
     try:
         conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
-        print('Postgres connected')
+        print('Postgres connected', user)
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT token FROM users WHERE user_name = '%s'" % user)
+        result = cur.fetchone()
+        print(result)
+        cur.close()
+        conn.close()
+        return result
     except psycopg2.Error as e:
         print('Postgres failed:{0}'.format(e))
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("SELECT * FROM users WHERE user = %s" % user)
-    result = cur.fetchone()    
-    cur.close()
-    conn.close()    
-    return result
+        return 'Error'
 
 def create_user(user, token):
     try:
@@ -20,7 +22,7 @@ def create_user(user, token):
     except psycopg2.Error as e:
         print('Postgres failed:{0}'.format(e))
     cur = conn.cursor()
-    cur.execute("INSERT INTO users(id, login, token, disabled) VALUES (DEFAULT, '{0}', '{1}', DEFAULT)".format(user, token))
+    cur.execute("INSERT INTO users(id, user_name, token, disabled) VALUES (DEFAULT, '{0}', '{1}', DEFAULT)".format(user, token))
     conn.commit()
     cur.close()
     conn.close()
