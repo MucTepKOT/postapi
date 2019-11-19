f8d87836fb371c852b351978f2c97a2a074a775e
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-20 00:28:38 +0300

    rewrite module with passlib

--- keygen.py
+++ keygen.py
@@ -1,16 +1,8 @@
 import uuid
-import hashlib
+from passlib.hash import pbkdf2_sha256
 
-def hashText(text):
-    """
-        Basic hashing function for a text using random unique salt.  
-    """
-    salt = uuid.uuid4().hex
-    return hashlib.sha256(salt.encode() + text.encode()).hexdigest() + salt
+def hash(text):
+    return pbkdf2_sha256.hash(text)
     
-def matchHashedText(hashedText, providedText):
-    """
-        Check for the text in the hashed text
-    """
-    _hashedText, salt = hashedText.split('')
-    return _hashedText == hashlib.sha256(salt.encode() + providedText.encode()).hexdigest()
+def verify_hash(text, hashed_text):
+    return pbkdf2_sha256.verify(text, hashed_text)
