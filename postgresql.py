d5922bfc0319f22af027ceb5caddf303610ba954
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-23 01:46:50 +0300

    add check pass and update token functions

--- postgresql.py
+++ postgresql.py
@@ -21,9 +21,9 @@ def create_user(user, password, token):
     except psycopg2.Error as err:
         print('Postgres failed:{0}'.format(err))
     cur = conn.cursor()
-    timestamp = time.time()
+    current_time = int(time.time())
     try:
-        cur.execute("INSERT INTO users(id, user_name, password, token, time) VALUES (DEFAULT, '{0}', '{1}', '{2}', {3})".format(user, password, token, timestamp))
+        cur.execute("INSERT INTO users(id, user_name, password, token, time) VALUES (DEFAULT, '{0}', '{1}', '{2}', {3})".format(user, password, token, current_time))
         conn.commit()
         return token
     except psycopg2.Error as err:
@@ -47,14 +47,14 @@ def get_token(user):
 
 def token_alive(token):
     try:
-        current_time = int(time.time())
         conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute("SELECT time FROM users WHERE token = '%s'" % token)
         result = cur.fetchone()
         cur.close()
         conn.close()
-        if current_time - result[0] > 3600:
+        current_time = int(time.time())
+        if current_time - result[0] > 60:
             return False
         else:
             return True
@@ -62,15 +62,30 @@ def token_alive(token):
         print('Postgres failed:{0}'.format(err))
         return 'Error'
 
+def update_token(user, token):
+    try:
+        conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        current_time = int(time.time())
+        cur.execute("UPDATE users SET token = '%s' WHERE user_name = '%s'" % (token, user))
+        cur.execute("UPDATE users SET time = '%s' WHERE user_name = '%s'" % (current_time, user))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return 'Success'        
+    except psycopg2.Error as err:
+        print('Postgres failed:{0}'.format(err))
+        return 'Error'
 
-
-    
-
-# try:
-#     conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
-#     print('Postgres connected')
-# except psycopg2.Error as e:
-#     print('Postgres failed:{0}'.format(e))
-# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-# cur.close()
-# conn.close()
+def check_password(user):
+    try:
+        conn = psycopg2.connect(dbname='postapi_security', user='muctepkot', password='muctepkot', host='localhost')
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT password FROM users WHERE user_name = '%s'" % user)
+        result = cur.fetchone()
+        cur.close()
+        conn.close()
+        return result[0]
+    except psycopg2.Error as err:
+        print('Postgres failed:{0}'.format(err))
+        return 'Error'
