7a376bcec7596d27a97ee81b43844a2b7a9ea639
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-27 01:24:42 +0300

    add new function 'connect_mongo'

--- mongo.py
+++ mongo.py
@@ -1,11 +1,13 @@
 import pymongo
 
-mongo_atlas = "mongodb+srv://muctepkot:pass@kot-elknu.gcp.mongodb.net/test?retryWrites=true&w=majority"
+def connect_mongo():
+    mongo_atlas = "mongodb+srv://muctepkot:pass@kot-elknu.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(mongo_atlas)
+    return client
 
 def insert_db(db, collections, **kwargs):
     try:
-        client = pymongo.MongoClient(mongo_atlas)
-        connect_db = client[db][collections]
+        connect_db = connect_mongo()[db][collections]
         connect_db.insert_one(kwargs).inserted_id
         return 'Success'
     except pymongo.errors.PyMongoError as err:
@@ -13,8 +15,7 @@ def insert_db(db, collections, **kwargs):
 
 def my_prediction(db, collections, user):
     try:
-        client = pymongo.MongoClient(mongo_atlas)
-        connect_db = client[db][collections]
+        connect_db = connect_mongo()[db][collections]
         db_answer = connect_db.find({'user_name' : user}, {'_id': False})
         return list(db_answer)
     except pymongo.errors.PyMongoError as err:
