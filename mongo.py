38bd45c6bf56f91423e776390cc67ed1ba4cb4be
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2019-11-26 23:13:49 +0300

    rewrite module for scaling in future

--- mongo.py
+++ mongo.py
@@ -1,27 +1,21 @@
 import pymongo
-from datetime import datetime
 
-def insert_db(user, data):
-    timestamp = datetime.timestamp(datetime.now())
-    data['timestamp'] = int(timestamp)
-    data['user_name'] = user
-    try:
-        pred.insert_one(data).inserted_id
-        return 'Success'        
-    except pymongo.errors.PyMongoError as e: 
-        return 'Here is some error: %s' % e
+mongo_atlas = "mongodb+srv://muctepkot:pass@kot-elknu.gcp.mongodb.net/test?retryWrites=true&w=majority"
 
-def my_prediction(user):
+def insert_db(db, collections, **kwargs):
     try:
-        db_answer = pred.find({'user_name' : user}, {'_id': False})
+        client = pymongo.MongoClient(mongo_atlas)
+        connect_db = client[db][collections]
+        connect_db.insert_one(kwargs).inserted_id
+        return 'Success'
+    except pymongo.errors.PyMongoError as err:
+        return 'Here is some error: %s' % err
+
+def my_prediction(db, collections, user):
+    try:
+        client = pymongo.MongoClient(mongo_atlas)
+        connect_db = client[db][collections]
+        db_answer = connect_db.find({'user_name' : user}, {'_id': False})
         return list(db_answer)
-    except pymongo.errors.PyMongoError as e:
-        return 'Here is some error: %s' % e
-
-try:
-    client = pymongo.MongoClient("mongodb+srv://muctepkot:pass@kot-elknu.gcp.mongodb.net/test?retryWrites=true&w=majority")
-    db = client.mongodb
-    pred = db.predictions
-    print('Database connected')
-except pymongo.errors.PyMongoError(message='pizda') as e:
-    print('Here is some error: %s' % e)
+    except pymongo.errors.PyMongoError as err:
+        return 'Here is some error: %s' % err
